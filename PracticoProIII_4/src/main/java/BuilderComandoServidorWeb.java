e547d66c3a37977e5041a4443df4320304837179
Oscar <oscardavidmur@hotmail.com>
2023-10-08 18:17:31 -0400

    Basico Server Web

--- PracticoProIII_4/src/main/java/BuilderComandoServidorWeb.java
+++ PracticoProIII_4/src/main/java/BuilderComandoServidorWeb.java
@@ -1,13 +1,16 @@
-public class BuilderComandoServidorWeb {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class BuilderComandoServidorWeb {
+    private final String OPERACION_REGEX =
+            "^get\/(s[0-9])\/([a-z]+)-([0-9]+)-([0-9]+)-([0-9]+)-([0-9]+)$";
     private BuilderComandoServidorWeb() {
     }
-
     public static ComandoServidorWeb crear(String cmdGet) {
         if (cmdGet == null || cmdGet.isEmpty()) {
           //  return new ComandoVacio(cmdGet);
         }
-
+        if (cmdGet.trim().equals(""))
         if (cmdGet.trim().equals("/tabla")) {
            // return new ComandoTabla(cmdGet);
         }
